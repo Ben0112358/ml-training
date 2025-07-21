5e89056225ec0a3c87692e8ee96f32c8a7aed6ef
Ben0112358 <benjaminringdahl@gmail.com>
2025-07-21 12:20:07 +0200

    last few files for our dockerization

--- src/ml_training/dummy_project/__main__.py
+++ src/ml_training/dummy_project/__main__.py
@@ -1,28 +1,18 @@
-from ml_training.config import MODEL_DIR, CLEAN_DATA_DIR, LOGS_DIR
 import logging
-from datetime import datetime
-from sklearn.linear_model import LogisticRegression
-import pandas as pd
-import joblib
+import subprocess
+import os
 from ml_training.utils import setup_logging
 
 
 def main():
     logger = logging.getLogger(__name__)
+    logger.info("Running docker according to the docker-compose.yaml file.")
 
-    logger.info("Gathering raw data")
-    df = pd.read_csv(CLEAN_DATA_DIR / "data.csv")
+    env = os.environ.copy()
+    if "ML_HOMELAB_ROOT" not in env:
+        raise RuntimeError("ML_HOMELAB_ROOT is not set in the environment.")
 
-    logger.info("Splitting into X, y")
-    X = df.iloc[:, 0].to_frame()
-    y = df["target"]
-
-    logger.info("Fitting model")
-    mdl = LogisticRegression()
-    mdl.fit(X, y)
-
-    logger.info("Saving model")
-    joblib.dump(mdl, MODEL_DIR / "model.pkl")
+    subprocess.run(["docker", "compose", "up", "--build"], env=env, check=True)
 
 
 if __name__ == "__main__":
