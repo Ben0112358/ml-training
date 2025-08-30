da680de7f5adfa99ec761641f8e1f13be498603a
Ben0112358 <benjaminringdahl@gmail.com>
2025-08-30 21:31:02 +0200

    simple initial training.py done

--- src/ml_training/investing_allocation_optimizer/training.py
+++ src/ml_training/investing_allocation_optimizer/training.py
@@ -1,9 +1,12 @@
 import logging
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+import numpy as np
 import joblib
 from ml_training.config import CLEAN_DATA_DIR, MODEL_DIR, ENV_VAR_OUTPUT_SUFFIX
 from ml_training.utils import setup_logging
+from ml_training.investing_allocation_optimizer.utils import (
+    BootstrapPortfolioOptimizer,
+)
 
 
 def main():
@@ -12,13 +15,11 @@ def main():
     logger.info("Gathering raw data")
     df = pd.read_csv(CLEAN_DATA_DIR / f"data_{ENV_VAR_OUTPUT_SUFFIX}.csv")
 
-    logger.info("Splitting into X, y")
-    X = df.iloc[:, 0].to_frame()
-    y = df["target"]
-
     logger.info("Fitting model")
-    mdl = LogisticRegression()
-    mdl.fit(X, y)
+    mdl = BootstrapPortfolioOptimizer(
+        metric=lambda x: np.mean(x),
+    )
+    mdl.fit(df)
 
     logger.info("Saving model")
     joblib.dump(mdl, MODEL_DIR / f"model_{ENV_VAR_OUTPUT_SUFFIX}.pkl")
